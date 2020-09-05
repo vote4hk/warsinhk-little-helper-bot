340af5e21fe72f21eb00e099b489907bdd888a49
Brian Leung <brianleung1017@gmail.com>
2020-09-05 17:04:03 +0800

    🔑feat: Check location by case no
    commit 2435420584c3ead938ef14dbc40bdb7115e00884
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Sep 5 17:03:05 2020 +0800
    
        update console.log
    
    commit 4ad5cc6274cd4cbbe85110a354d6a65dc1b21c78
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Sep 5 17:01:20 2020 +0800
    
        rename command
    
    commit fac3828eb7c891659fa630f5fb424d155b7128ad
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Sep 5 16:59:11 2020 +0800
    
        add chat flow
    
    commit 4a1d5a264eba5c7084e7da37fdd5ce4ccc88816d
    Author: Brian Leung <brianleung1017@gmail.com>
    Date:   Sat Sep 5 16:35:30 2020 +0800
    
        Add filter location by case no feature

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const Markup = require("telegraf/markup");
 const Extra = require("telegraf/extra");
 
 const recon = require("./recon");
+const { checkCaseLocationByCaseNo } = require("./query");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const showWelcomeMessage = (ctx, onStart = false) => {
+  const onStartMessage = onStart
+    ? "<b>歡迎使用 wars.vote4.hk \n🤒武漢肺炎民間資訊😷小幫手🕵️‍♀️</b>\n利申第一次寫Telegram Bot，請高抬貴手🙇‍♂️"
+    : "有咩幫到你🕵️‍♀️？";
+  return ctx.reply(
+    onStartMessage,
+    Extra.HTML().markup(
+      Markup.inlineKeyboard([
+        Markup.callbackButton("入 Case No 揾行蹤", "checkCaseLocation"),
+        Markup.callbackButton("比較我哋同CHP", "compareCHPandWARS"),
+      ])
+    )
+  );
+};
+bot.start((ctx) => {
+  showWelcomeMessage(ctx, true);
+});
 
 bot.use(async (ctx, next) => {
   const start = new Date();
@@ -14,20 +33,51 @@ bot.use(async (ctx, next) => {
   console.log("Response time: %sms", ms);
 });
 
-bot.start((ctx) => {
-  ctx.reply(
-    "<b>歡迎使用 wars.vote4.hk 🤒武漢肺炎民間資訊😷小幫手</b>\n利申第一次寫Telegram Bot，請多多包涵🙇‍♂️",
-    Extra.HTML().markup(
-      Markup.inlineKeyboard([
-        Markup.callbackButton(
-          "比較我哋同CHP",
-          "check_missing_location_from_chp"
-        ),
-      ])
-    )
-  );
+bot.action("checkCaseLocation", async (ctx) => {
+  try {
+    ctx.reply(`請打Case No（只限數字...）\n`);
+
+    bot.on("text", async (ctx) => {
+      const query = +ctx.message.text;
+      if (!Number.isInteger(query)) {
+        ctx.reply(`🔢淨係可以打Number`);
+        return showWelcomeMessage(ctx);
+      }
+
+      ctx.reply(`🕵️‍♀️幫緊你～幫緊你～💦`);
+
+      const { locations, error } = await checkCaseLocationByCaseNo(query);
+
+      if (error) {
+        ctx.reply(`中伏，有bug!`);
+        return showWelcomeMessage(ctx);
+      }
+
+      if (!locations.length) {
+        ctx.reply(`咩都揾唔到😩`);
+        return showWelcomeMessage(ctx);
+      }
+
+      let locationText = "";
+      for (const [i, location] of locations.entries()) {
+        locationText += `${location.end_date || `202?-??-?? `} | ${
+          location.action_zh
+        } | ${location.sub_district_zh} | ${location.location_zh}\n`;
+      }
+
+      ctx.reply(
+        `個案編號：${locations[0].case_no}\n最後出現     | 行蹤 | 地區 | 地點\n${locationText}`
+      );
+      return showWelcomeMessage(ctx);
+    });
+  } catch (error) {
+    ctx.reply(`中伏，有bug!`);
+    console.error(error.stack);
+    return showWelcomeMessage(ctx);
+  }
 });
-bot.action("check_missing_location_from_chp", async (ctx) => {
+
+bot.action("compareCHPandWARS", async (ctx) => {
   try {
     ctx.reply(
       `等朕check下我哋同CHP過往14日地址有咩分別先......(咩都唔好撳，等1-2分鐘先)\n`
@@ -39,7 +89,8 @@ bot.action("check_missing_location_from_chp", async (ctx) => {
     } = await recon.checkMissingGovLocation();
 
     if (error) {
-      return ctx.reply(`中伏，有bug!`);
+      ctx.reply(`中伏，有bug!`);
+      return showWelcomeMessage(ctx);
     }
 
     ctx.reply(`以下可能係政府有我哋無，亦可能係False Alarm，例如佢哋串錯字\n`);
@@ -49,13 +100,15 @@ bot.action("check_missing_location_from_chp", async (ctx) => {
         notMatch.location_zh || notMatch.location_en
       }\n`;
     }
-    ctx.reply(unMatchText);
     ctx.reply(
-      `我哋有幾多地址: ${CaseLocationData.length} || 有幾多唔中: ${notMatchArray.length}`
+      `${unMatchText}我哋有幾多地址: ${CaseLocationData.length} || 有幾多唔中: ${notMatchArray.length}`
     );
+    return showWelcomeMessage(ctx);
   } catch (error) {
     ctx.reply(`中伏，有bug!`);
-    return console.error(error.stack);
+    console.error(error.stack);
+    return showWelcomeMessage(ctx);
   }
 });
+
 bot.launch();
